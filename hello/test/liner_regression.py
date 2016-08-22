6c533ac665025926de67d260d530e3560ba41761
HongyuePei <hongyuepei@datamesh.com.cn>
2016-08-22 18:16:49 +0800

    liner_regression

---
+++ hello/test/liner_regression.py
@@ -0,0 +1,57 @@
+ # -*- coding:utf-8 -*-
+'''
+Created on 2016年8月22日
+
+@author: Administrator
+线性回归算法
+'''
+import pandas
+import matplotlib.pyplot as plt
+from sklearn import datasets, linear_model
+
+def get_data(file_name):
+    data = pandas.read_csv(file_name)
+    X_parameter = []
+    Y_parameter = []
+#     "TV","Radio","Newspaper","Sales"
+    for TV, Radio, Newspaper, Sales in zip(data['TV'], data['Radio'], data['Newspaper'], data['Sales']):
+        X_parameter_sub = []
+        X_parameter_sub.append(TV)
+        X_parameter_sub.append(Radio)
+        X_parameter_sub.append(Newspaper)
+        X_parameter.append(X_parameter_sub)
+        Y_parameter.append(Sales)
+
+    return X_parameter, Y_parameter    
+
+# Function for Fitting our data to Linear model
+def linear_model_main(X_parameters,Y_parameters,predict_value):
+    # Create linear regression object
+    regr = linear_model.LinearRegression()
+    regr.fit(X_parameters,Y_parameters)
+    predict_outcome = regr.predict(predict_value)
+    predictions = {}
+    predictions['intercept'] = regr.intercept_
+    predictions['coefficient'] = regr.coef_
+    predictions['predicted_value'] = predict_outcome
+    return predictions
+
+
+X, Y = get_data(r'D:\Advertising.csv')
+print X
+# print Y
+predictvalue = [232.1,8.6,8.7]
+# print linear_model_main(X, Y, predictvalue)
+
+Y_Predict = []
+for x in X:
+    Y_Predict.append(linear_model_main(X, Y, x).get('predicted_value'))
+
+print Y_Predict
+
+
+
+
+
+
+
