3c697076943701bc5044536686cbd2507307a500
HongyuePei <hongyuepei@datamesh.com.cn>
2016-08-24 14:25:45 +0800

    a

--- hello/test/liner_regression_m.py
+++ hello/test/liner_regression_m.py
@@ -5,4 +5,13 @@ Created on 2016年8月24日
 '''
 import numpy
 
-print 'hello, world'
+def get_data(file_name):
+    a = chr(32)
+    lines = open(file_name)
+    X_parameter = []
+    Y_parameter = []
+    for line in lines.readline():
+        x = line.strip().split(a)
+        
+    
+get_data('')
